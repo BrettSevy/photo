ce89f0becf80522a748b4173890068089a655f24
Brett Sevy <justbrettt@gmail.com>
2021-11-29 23:40:52 -0600

    deploy

--- .vscode/precache-manifest.b583a7f7f89deb55a5464aa3a9445e14.js
+++ .vscode/precache-manifest.b583a7f7f89deb55a5464aa3a9445e14.js
@@ -1,58 +1,174 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7c406315ed8b3094b86cc59321cb0699",
+    "revision": "07a9279c398562a19a34a59981c2ada7",
     "url": "/index.html"
   },
   {
-    "revision": "0be667bfadb2090fb2c9",
-    "url": "/static/css/2.e4346227.chunk.css"
+    "revision": "ffddf529c26929d091b8",
+    "url": "/static/css/main.85eacb53.chunk.css"
   },
   {
-    "revision": "3674de7a9a2c03b01a1a",
-    "url": "/static/css/main.1577891c.chunk.css"
+    "revision": "3d1eeef4f6d2bad72b8e",
+    "url": "/static/js/2.a307e6ef.chunk.js"
   },
   {
-    "revision": "0be667bfadb2090fb2c9",
-    "url": "/static/js/2.08fd878a.chunk.js"
+    "revision": "c31a94e7aad63e320e4529cc5074d033",
+    "url": "/static/js/2.a307e6ef.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
-    "url": "/static/js/2.08fd878a.chunk.js.LICENSE.txt"
+    "revision": "ffddf529c26929d091b8",
+    "url": "/static/js/main.2ad00e80.chunk.js"
   },
   {
-    "revision": "3674de7a9a2c03b01a1a",
-    "url": "/static/js/main.024277c7.chunk.js"
+    "revision": "09cd470e8342bc71b82b",
+    "url": "/static/js/runtime-main.6158263c.js"
   },
   {
-    "revision": "310aaeac4bc0b0364ef8",
-    "url": "/static/js/runtime-main.582fb2fb.js"
+    "revision": "1e01a1709762d7b4520e6429c5118323",
+    "url": "/static/media/1.1e01a170.jpg"
   },
   {
-    "revision": "9b1d9f180e0dda24c7715a9009d5554b",
-    "url": "/static/media/burgers.9b1d9f18.png"
+    "revision": "9195816f0b1ac4f234fbaa471532b0df",
+    "url": "/static/media/10.9195816f.jpg"
   },
   {
-    "revision": "086246a59deea324a92cc52197b273cf",
-    "url": "/static/media/coin.086246a5.jpg"
+    "revision": "f72b27b165e03730fd4d9bf7ab81c105",
+    "url": "/static/media/11.f72b27b1.jpg"
   },
   {
-    "revision": "8623edab5cad78c2e753474b3caa8dc9",
-    "url": "/static/media/fitness.8623edab.png"
+    "revision": "be9132ef203779070e3e58310da2cb85",
+    "url": "/static/media/12.be9132ef.jpg"
   },
   {
-    "revision": "742cefed97d3123c8e103f52190a2b33",
-    "url": "/static/media/kobe.742cefed.png"
+    "revision": "bfd9e6ad21c4be89011ae81ee566ca1c",
+    "url": "/static/media/13.bfd9e6ad.jpg"
   },
   {
-    "revision": "f7c36b56a85c88c69192a8994b2deaef",
-    "url": "/static/media/planner.f7c36b56.png"
+    "revision": "434660513a6e282a0a944e371edd180e",
+    "url": "/static/media/14.43466051.jpg"
   },
   {
-    "revision": "e44da7451f5e778fb0d7e5d73f1209e4",
-    "url": "/static/media/stocks.e44da745.png"
+    "revision": "f85b82eb94007ce2aa3086b651f388e3",
+    "url": "/static/media/2.f85b82eb.jpg"
   },
   {
-    "revision": "3cc4d84f594c7836caf6d2359a4f3c8d",
-    "url": "/static/media/weather.3cc4d84f.png"
+    "revision": "ece1c9d3e2ca8d1dcf5bff3c02393de0",
+    "url": "/static/media/3.ece1c9d3.jpg"
+  },
+  {
+    "revision": "c57f9150d76f1bc1376b909d567031f6",
+    "url": "/static/media/4.c57f9150.jpg"
+  },
+  {
+    "revision": "ee0e45d310f87f6a866540db451f79c4",
+    "url": "/static/media/5.ee0e45d3.jpg"
+  },
+  {
+    "revision": "47a2c9e9a6b08421dddb81f0a3167645",
+    "url": "/static/media/6.47a2c9e9.jpg"
+  },
+  {
+    "revision": "2ef9a9a24b46c1fae29cfd1ff800f612",
+    "url": "/static/media/7.2ef9a9a2.jpg"
+  },
+  {
+    "revision": "f4d3d2425379249e8ab8cf1918260859",
+    "url": "/static/media/8.f4d3d242.jpg"
+  },
+  {
+    "revision": "ff2a8cd63940dbdafa6eb85a195437bb",
+    "url": "/static/media/9.ff2a8cd6.jpg"
+  },
+  {
+    "revision": "4195cf989fac0128a89669f40a1e3496",
+    "url": "/static/media/bootstrap.4195cf98.png"
+  },
+  {
+    "revision": "06f2a24ebef4c7e9d96cd9912c38851a",
+    "url": "/static/media/burgers.06f2a24e.png"
+  },
+  {
+    "revision": "c88a64259cfe0e2aef48cafe97398c1c",
+    "url": "/static/media/coinflip.c88a6425.png"
+  },
+  {
+    "revision": "9fe7584b6d29bec74a66024829929697",
+    "url": "/static/media/css.9fe7584b.png"
+  },
+  {
+    "revision": "2ce1a5665a490a810b4ce7f8aa17aaca",
+    "url": "/static/media/fitness.2ce1a566.png"
+  },
+  {
+    "revision": "f02dc5cbdbf8ae0fe9db060f31bd74e5",
+    "url": "/static/media/googlebooks.f02dc5cb.png"
+  },
+  {
+    "revision": "2ea01c0e92883b6e481dd2e62f5e6d4e",
+    "url": "/static/media/html.2ea01c0e.png"
+  },
+  {
+    "revision": "a58728919c10c74e25a9b656c478f730",
+    "url": "/static/media/jquery.a5872891.png"
+  },
+  {
+    "revision": "f3c040f269f349736c31911f7922e0a6",
+    "url": "/static/media/js.f3c040f2.png"
+  },
+  {
+    "revision": "c00129be5a29d1bf10051657df17b23c",
+    "url": "/static/media/mongodb.c00129be.png"
+  },
+  {
+    "revision": "96f1ee4bf2c0af9e5b09c57303dd2dd8",
+    "url": "/static/media/mysql.96f1ee4b.png"
+  },
+  {
+    "revision": "5b47d4f8d197911857e8e77cb4dd1569",
+    "url": "/static/media/node.5b47d4f8.png"
+  },
+  {
+    "revision": "d908864f60fb46d3c244ae3e7bc1b5c4",
+    "url": "/static/media/notetaker.d908864f.png"
+  },
+  {
+    "revision": "c3a9a451280ba33e0984cb8245c56e68",
+    "url": "/static/media/parallax.c3a9a451.jpeg"
+  },
+  {
+    "revision": "9b634d8ced07edee87cd55c8e6fb787a",
+    "url": "/static/media/password.9b634d8c.png"
+  },
+  {
+    "revision": "8a93b01ea08f79d92139661d824bbef5",
+    "url": "/static/media/planner.8a93b01e.png"
+  },
+  {
+    "revision": "5f74b5ea8ed141f690afec982e06e398",
+    "url": "/static/media/portrait.5f74b5ea.JPG"
+  },
+  {
+    "revision": "abee70ce5f38f3bdd19e2f91541a7504",
+    "url": "/static/media/quiz.abee70ce.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "b5cecb993869998a5dfa9bb9c18870dc",
+    "url": "/static/media/stocks.b5cecb99.png"
+  },
+  {
+    "revision": "1858e5f7d3f520f85adef5797eeee66b",
+    "url": "/static/media/teamgenerator.1858e5f7.png"
+  },
+  {
+    "revision": "bc44411bd665571f0006f202cbba40ad",
+    "url": "/static/media/weather.bc44411b.png"
+  },
+  {
+    "revision": "50fad6986a4ce36740fdf8aacf19af1f",
+    "url": "/static/media/whiskey.50fad698.png"
   }
 ]);
--- .vscode/service-worker.js
+++ .vscode/service-worker.js
@@ -11,29 +11,30 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+ importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts(
-  "/precache-manifest.b583a7f7f89deb55a5464aa3a9445e14.js"
-);
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
-});
+ importScripts(
+   "/precache-manifest.45624194939f5e9b273091a64a8dafcb.js"
+ );
+ 
+ self.addEventListener('message', (event) => {
+   if (event.data && event.data.type === 'SKIP_WAITING') {
+     self.skipWaiting();
+   }
+ });
+ 
+ workbox.core.clientsClaim();
+ 
+ /**
+  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+  * requests for URLs in the manifest.
+  * See https://goo.gl/S9QRab
+  */
+ self.__precacheManifest = [].concat(self.__precacheManifest || []);
+ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+ 
+ workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+   
+   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+ });
+ 
