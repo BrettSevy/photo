96fbd2877c4f5af215cda5b15bd33861fc57c9aa
Brett Sevy <justbrettt@gmail.com>
2021-12-07 20:57:28 -0600

    deploying

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import Main from './Components/Main';
-// import LandingPage from './Components/Pages/LandingPage';
-// import PhotographyPage from './Components/Pages/PhotographyPage';
-// import WebDevPage from './Components/Pages/WebDevPage';
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+// import { Route } from 'react-router'
+import LandingPage from './Components/Pages/LandingPage'
+import PhotographyPage from './Components/Pages/PhotographyPage'
+import WebDevPage from './Components/Pages/WebDevPage'
 
 
 export default function App() {
  
   return (
-    <div>
-      <Main/>
-      {/* <WebDevPage/>
-      <PhotographyPage/> */}
-      
-    </div>
-  );
+    
+      <div>
+          <Switch>
+              <Route exact path ="/" component={LandingPage}></Route>
+              
+              <Route path="/photography" component={PhotographyPage}></Route>
+
+              <Route path="/WebDev" component={WebDevPage}></Route>
+
+          </Switch>
+          
+      </div>
+  )
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,25 +1,25 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-// import { Route } from 'react-router'
-import LandingPage from './Pages/LandingPage'
-import PhotographyPage from './Pages/PhotographyPage'
-import WebDevPage from './Pages/WebDevPage'
+// import React from 'react'
+// import { Switch, Route } from 'react-router-dom'
+// // import { Route } from 'react-router'
+// import LandingPage from './Pages/LandingPage'
+// import PhotographyPage from './Pages/PhotographyPage'
+// import WebDevPage from './Pages/WebDevPage'
 
 
-function Main() {
-    return (
-        <div>
-            <Switch>
-                <Route exact path ="/" component={LandingPage}></Route>
+// function Main() {
+//     return (
+//         <div>
+//             <Switch>
+//                 <Route exact path ="/" component={LandingPage}></Route>
                 
-                <Route path="/photography" component={PhotographyPage}></Route>
+//                 <Route path="/photography" component={PhotographyPage}></Route>
 
-                <Route path="/WebDev" component={WebDevPage}></Route>
+//                 <Route path="/WebDev" component={WebDevPage}></Route>
 
-            </Switch>
+//             </Switch>
             
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default Main;
+// export default Main;
